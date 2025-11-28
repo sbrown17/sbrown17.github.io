0a4e43e1a388741b27b295e6d5e3621dacbe2aa4
Stuart Brown <stubrown357@gmail.com>
2025-11-28 21:15:37 +0000

    remove logos

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import { useState, useEffect } from "react";
 import { APITester } from "./APITester";
 import "./index.css";
 
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
-
 interface BlogPost {
   slug: string;
   title: string;
@@ -22,15 +19,7 @@ export function App() {
 
   return (
     <div className="app">
-      <div className="logo-container">
-        <img src={logo} alt="Bun Logo" className="logo bun-logo" />
-        <img src={reactLogo} alt="React Logo" className="logo react-logo" />
-      </div>
-
       <h1>Welcome</h1>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
 
       <section style={{ marginTop: "2rem" }}>
         <h2>Blog Posts</h2>
